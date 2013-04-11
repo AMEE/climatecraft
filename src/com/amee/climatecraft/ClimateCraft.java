df57bc7e5811eb20ee1fe98257d0141bda226f6b
James Smith <james@floppy.org.uk>
2013-04-11 21:27:35 +0100

    Add skeleton onEnable and onDisable

--- src/com/amee/climatecraft/ClimateCraft.java
+++ src/com/amee/climatecraft/ClimateCraft.java
@@ -4,4 +4,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class ClimateCraft extends JavaPlugin {
 
+	@Override
+	public void onEnable(){
+		// TODO Insert logic to be performed when the plugin is enabled
+	}
+	 
+	@Override
+	public void onDisable() {
+		// TODO Insert logic to be performed when the plugin is disabled
+	}
+
 }
