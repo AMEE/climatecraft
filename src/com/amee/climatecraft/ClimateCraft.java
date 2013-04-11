bb902d89979d6f2996fdce7765367c443e752bfc
James Smith <james@floppy.org.uk>
2013-04-11 21:29:54 +0100

    Add a basic co2 command to print the co2 value

--- src/com/amee/climatecraft/ClimateCraft.java
+++ src/com/amee/climatecraft/ClimateCraft.java
@@ -1,5 +1,7 @@
 package com.amee.climatecraft;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ClimateCraft extends JavaPlugin {
@@ -12,6 +14,14 @@ public class ClimateCraft extends JavaPlugin {
 	@Override
 	public void onDisable() {
 		// TODO Insert logic to be performed when the plugin is disabled
+	}
+
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+		if(cmd.getName().equalsIgnoreCase("co2")){
+			sender.sendMessage(Atmosphere.totalAsString());
+			return true;
+		}
+		return false; 
 	}
 
 }
